92e53fea07769b2de9bea3b674d9cb448ab648ce
Rich Morgan <ionux@users.noreply.github.com>
2015-06-18 11:51:54 -0400

    Updated formatting for PSR compliance.

--- modules/bitpay/validation.php
+++ modules/bitpay/validation.php
@@ -13,23 +13,29 @@ $cart     = $context->cart;
 
 $bitpay   = new bitpay();
 
-if ($cart->id_customer == 0 OR $cart->id_address_delivery == 0 OR $cart->id_address_invoice == 0 OR !$bitpay->active)
-        Tools::redirect('index.php?controller=order&step=1');
+if ($cart->id_customer == 0 ||
+    $cart->id_address_delivery == 0 ||
+    $cart->id_address_invoice == 0 ||
+    !$bitpay->active)
+{
+    Tools::redirect('index.php?controller=order&step=1');
+}
 
 // Check that this payment option is still available in case the customer
 // changed his address just before the end of the checkout process
 $authorized = false;
 
-foreach (Module::getPaymentModules() as $module)
-        if ($module['name'] == 'bitpay') {
-                $authorized = true;
-                break;
-        }
+foreach (Module::getPaymentModules() as $module) {
+    if ($module['name'] == 'bitpay') {
+        $authorized = true;
+        break;
+    }
+}
 
-if (!$authorized)
-        die($bitpay->l('This payment method is not available.', 'validation'));
+if (!$authorized) {
+    die($bitpay->l('The BitPay payment method is not available.', 'validation'));
+}
 
 echo $bitpay->execPayment($cart);
 
 include_once(dirname(__FILE__).'/../../footer.php');
-
